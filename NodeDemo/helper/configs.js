4c4c9c52a4a3e05d135ec26118f88da384f4b9a7
hovinh414 <hovinh414@gmail.com>
2024-01-06 10:40:34 +0700

    send mail exercise 06/01

--- NodeDemo/helper/configs.js
+++ NodeDemo/helper/configs.js
@@ -1,4 +1,10 @@
 module.exports= {
     SECRET_KEY:"heheheehe",
-    EXP: '20d'
+    EXP: '20d',
+    cookieExp: 2,
+    MAIL_HOST:'sandbox.smtp.mailtrap.io',
+    MAIL_PORT:2525,
+    MAIL_USER: '085edaf546993c',
+    MAIL_PASS:'493969a6ceca76',
+
 }
